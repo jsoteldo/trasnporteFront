8eac2cf641e75257eec4bd0ac84c244152332527
jsoteldo <georsot@gmail.com>
2024-05-01 12:24:47 -0500

    fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,17 @@ export default defineConfig({
   mode: 'development',
   base:'https://jsoteldo.github.io/trasnporteFront',
   plugins: [react(), basicSsl(),svgr()],
-  build: { chunkSizeWarningLimit: 1600, },
+  build: { 
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output:{
+          manualChunks(id) {
+              if (id.includes('node_modules')) {
+                  return id.toString().split('node_modules/')[1].split('/')[0].toString();
+              }
+          }
+      }
+  } },
   resolve: {
     alias: {
       '@app': path.resolve(__dirname, './src'),
